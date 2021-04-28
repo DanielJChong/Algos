a59ccfdd9609d566aa83262fcaaa5a9c8e49d48a
Daniel Chong <danieljchong@gmail.com>
2021-04-28 15:06:50 -0700

    added algos from 10/20/2020

---
+++ wf_algos/algo10-20-2020.js
@@ -0,0 +1,25 @@
+10/20/2020
+/*
+    1.) 
+    Recreate the pop method without using .pop(). 
+    Be sure to return the element that was popped
+*/
+
+var myArr = [5, 6, 7, 8]
+
+var poppedNum = myArr.pop()
+console.log(poppedNum);
+
+
+/* 
+    2.) 
+    Array: Pop Front
+    Given array, remove and return the value at the beginning 
+    of the array. Do this without using any built-in array 
+    methods except pop() .
+*/
+
+
+var myArr = [5, 6, 7, 8]
+var poppedElement = popFront(myArr);
+
